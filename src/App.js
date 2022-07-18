f5397290f921a69d004fd22fe8a08b5d56d3ac17
ZABILA PETRO <petrozabila@gmail.com>
2022-07-18 16:57:04 +0300

    homepage changed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import user from '.user.json';
+import Profile from './Components/Profile';
+
+export default function App(user) {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+import user from '.user.json';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
